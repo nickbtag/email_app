211d4e826cb74d193e55c34371404d5a2c68e48a
nickbtag <battaglia@cua.edu>
2022-06-07 23:10:14 -0400

    save

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def data():
     if request.method == 'POST':
         file = request.files['upload-file']
         filename = file.filename
-        
+        file.save(file.filename)
         with open(file.filename) as f:
             print(f)
             for i in csv.DictReader(f):
